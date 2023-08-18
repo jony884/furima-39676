1aa36308f24ff96b824dd8f050de898327bdaad1
jony884 <139261229+jony884@users.noreply.github.com>
2023-08-18 17:32:38 +0900

    fix 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
 
   validates :nick_name, presence: true
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9]).{6,}\z/, message: "は英字と数字を含む6文字以上で設定してください" }
-  validates :family_name_kanji, format: { with: /\A[一-龯ぁ-んァ-ンー－々ヶ]+\z/, message: "は漢字、カタカナ、ひらがなのみで入力してください" }
-  validates :personal_name_kanji, format: { with: /\A[一-龯ぁ-んァ-ンー－々ヶ]+\z/, message: "は漢字、カタカナ、ひらがなのみで入力してください" }
+  validates :family_name_kanji, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は漢字、カタカナ、ひらがなのみで入力してください" }
+  validates :personal_name_kanji, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は漢字、カタカナ、ひらがなのみで入力してください" }
   validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナのみで入力してください" }
   validates :personal_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナのみで入力してください" }
   validates :birthdate,presence: true
