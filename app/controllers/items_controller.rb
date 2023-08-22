47d9ceb7bf335df579988ea0cfa40c1628cfc901
jony884 <139261229+jony884@users.noreply.github.com>
2023-08-22 16:56:39 +0900

    error message出力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,15 +11,13 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    unless @item.valid?
-      render :new and return
+    if @item.valid?
+       @item.save
+       redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
     end
-
-    @item.save
-    redirect_to root_path
   end
-
-
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,42 @@ class Item < ApplicationRecord
   belongs_to :shipping_origin
   belongs_to :days_to_ship
 
-  validates :category_id, :item_condition_id, :shipping_cost_id, :shipping_origin_id, :days_to_ship_id, presence: true, numericality: { other_than: 1 }
-  validates :item_name, presence: true, length: { maximum: 40 }
-  validates :item_description, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 999999 }
-  validates :image, presence: true
+  validates :category_id, 
+            presence: { message: "カテゴリーを選択してください" }, 
+            numericality: { other_than: 1, message: "カテゴリーを選択してください" }
+
+  validates :item_condition_id, 
+            presence: { message: "商品の状態を選択してください" }, 
+            numericality: { other_than: 1, message: "商品の状態を選択してください" }
+
+  validates :shipping_cost_id, 
+            presence: { message: "配送料の負担を選択してください" }, 
+            numericality: { other_than: 1, message: "配送料の負担を選択してください" }
+
+  validates :shipping_origin_id, 
+            presence: { message: "発送元の地域を選択してください" }, 
+            numericality: { other_than: 1, message: "発送元の地域を選択してください" }
+
+  validates :days_to_ship_id, 
+            presence: { message: "発送までの日数を選択してください" }, 
+            numericality: { other_than: 1, message: "発送までの日数を選択してください" }
+
+  validates :item_name, 
+            presence: { message: "商品名を入力してください" },
+            length: { maximum: 40, message: "商品名は40文字以下で入力してください" }
+
+  validates :item_description, 
+            presence: { message: "商品の説明を入力してください" },
+            length: { maximum: 1000, message: "商品の説明は1000文字以下で入力してください" }
+
+  validates :price, 
+            presence: { message: "価格を入力してください" },
+            numericality: { 
+              greater_than_or_equal_to: 300, 
+              less_than_or_equal_to: 999999, 
+              message: "価格は300以上999999以下で設定してください" 
+            }
+
+  validates :image, presence: { message: "商品画像を選択してください" }
+
 end
